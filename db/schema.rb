55ad423124a44d708c57fde041c6b3edf357afee
yyu8k <50798858+yyu8k@users.noreply.github.com>
2019-09-30 20:10:30 +0900

    fix:rake-db-reset

--- db/schema.rb
+++ db/schema.rb
@@ -22,15 +22,15 @@ ActiveRecord::Schema.define(version: 2019_09_24_101259) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "first_name", null: false
-    t.string "first_name_kana", null: false
-    t.string "last_name", null: false
-    t.string "last_name_kana", null: false
-    t.string "phone_number", null: false
-    t.date "birthday", null: false
-    t.string "sex", null: false
-    t.integer "height", null: false
-    t.integer "weight", null: false
+    t.string "first_name", default: "", null: false
+    t.string "first_name_kana", default: "", null: false
+    t.string "last_name", default: "", null: false
+    t.string "last_name_kana", default: "", null: false
+    t.string "phone_number", default: "", null: false
+    t.date "birthday"
+    t.string "sex", default: "", null: false
+    t.integer "height"
+    t.integer "weight"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
