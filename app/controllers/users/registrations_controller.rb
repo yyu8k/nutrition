965a49c3376f20f79e025fe17255e35008485c3d
yyu8k <50798858+yyu8k@users.noreply.github.com>
2019-09-30 20:10:09 +0900

    cancel:comment-out-of-devise-controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -14,6 +14,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
+  def new
+    super
+  end
+
+  def create
+    super
+  end
+  
   # GET /resource/edit
   # def edit
   #   super
@@ -54,6 +62,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_sign_up_path_for(resource)
   #   super(resource)
   # end
+
+  def after_sign_up_path_for(resource)
+    root_path
+  end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
