312dc5f4952c261679efb3e3f380dec2966c39c9
yyu8k <50798858+yyu8k@users.noreply.github.com>
2019-09-30 20:09:16 +0900

    remove:null-false-of-integer

--- db/migrate/20190924101259_devise_create_users.rb
+++ db/migrate/20190924101259_devise_create_users.rb
@@ -4,17 +4,17 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string  :email,              null: false, default: ""
+      t.string  :email,              null: false, default: "", unique: true
       t.string  :encrypted_password, null: false, default: ""
-      t.string  :first_name,         null: false
-      t.string  :first_name_kana,    null: false
-      t.string  :last_name,          null: false
-      t.string  :last_name_kana,     null: false
-      t.string  :phone_number,       null: false, unique: true
-      t.date    :birthday,           null: false
-      t.string  :sex,                null: false
-      t.integer :height,             null: false
-      t.integer :weight,             null: false
+      t.string  :first_name,         null: false, default: ""
+      t.string  :first_name_kana,    null: false, default: ""
+      t.string  :last_name,          null: false, default: ""
+      t.string  :last_name_kana,     null: false, default: ""
+      t.string  :phone_number,       null: false, default: "", unique: true
+      t.date    :birthday
+      t.string  :sex,                null: false, default: ""
+      t.integer :height
+      t.integer :weight
 
       ## Recoverable
       t.string   :reset_password_token
