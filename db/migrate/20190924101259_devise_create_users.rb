6819aec581525cc7e3c4580e1d8c1001561b2a0e
yyu8k <50798858+yyu8k@users.noreply.github.com>
2019-10-05 21:53:38 +0900

    fix:rake-db-migrate-reset

--- db/migrate/20190924101259_devise_create_users.rb
+++ db/migrate/20190924101259_devise_create_users.rb
@@ -4,17 +4,17 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string  :email,              null: false, default: "", unique: true
-      t.string  :encrypted_password, null: false, default: ""
-      t.string  :first_name,         null: false, default: ""
-      t.string  :first_name_kana,    null: false, default: ""
-      t.string  :last_name,          null: false, default: ""
-      t.string  :last_name_kana,     null: false, default: ""
-      t.string  :phone_number,       null: false, default: "", unique: true
-      t.date    :birthday
-      t.string  :sex,                null: false, default: ""
-      t.integer :height
-      t.integer :weight
+      t.string  :email,              null: false, unique: true
+      t.string  :encrypted_password, null: false
+      t.string  :first_name,         null: false
+      t.string  :first_name_kana,    null: false
+      t.string  :last_name,          null: false
+      t.string  :last_name_kana,     null: false
+      t.string  :phone_number,       null: false, unique: true
+      t.date    :birthday,           null: false
+      t.string  :sex,                null: false
+      t.string  :height,             null: false
+      t.string  :weight,             null: false
 
       ## Recoverable
       t.string   :reset_password_token
