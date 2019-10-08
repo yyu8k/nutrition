d27a9ef0e374157a9884fc4ceeda70309c544992
yyu8k <50798858+yyu8k@users.noreply.github.com>
2019-10-08 17:53:51 +0900

    add:foods-controller-search-action

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -8,4 +8,11 @@ class FoodsController < ApplicationController
     end
   end
 
+  def search
+    @foods = Food.where('name LIKE(?)', "%#{params[:keyword]}%") #paramsとして送られてきたkeyword（入力された語句）で、Gohanモデルのnameカラムを検索し、その結果を@gohansに代入する
+    respond_to do |format| 
+      format.json { render 'index', json: @foods } #json形式のデータを受け取ったら、@gohansをデータとして返す そしてindexをrenderで表示する
+    end
+  end
+
 end
