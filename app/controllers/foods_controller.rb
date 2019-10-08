a15a9fc6807cddbb13ead4823c29aaa5551618dd
yyu8k <50798858+yyu8k@users.noreply.github.com>
2019-10-08 17:50:30 +0900

    add:foods-controller-index-action

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,2 +1,11 @@
 class FoodsController < ApplicationController
+
+  def index
+    @foods = Gohan.where('name LIKE(?)', "%#{params[:keyword]}%")
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
 end
