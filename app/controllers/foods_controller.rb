56ab2b96bbf2deed883e7e532ccac66ce4740e23
yyu8k <50798858+yyu8k@users.noreply.github.com>
2019-10-08 17:54:21 +0900

    fix:misspelling

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,7 +1,7 @@
 class FoodsController < ApplicationController
 
   def index
-    @foods = Gohan.where('name LIKE(?)', "%#{params[:keyword]}%")
+    @foods = Food.where('name LIKE(?)', "%#{params[:keyword]}%")
     respond_to do |format|
       format.html
       format.json
