9ed3fa7baa44b59b6ac391d2e08ecb754cb0e738
yyu8k <50798858+yyu8k@users.noreply.github.com>
2019-10-21 23:35:07 +0900

    update:add-column

--- db/migrate/20191021143339_create_event_foods.rb
+++ db/migrate/20191021143339_create_event_foods.rb
@@ -1,7 +1,8 @@
 class CreateEventFoods < ActiveRecord::Migration[5.2]
   def change
     create_table :event_foods do |t|
-
+      t.references :event, foreign_key: true
+      t.references :food, foreign_key: true
       t.timestamps
     end
   end
