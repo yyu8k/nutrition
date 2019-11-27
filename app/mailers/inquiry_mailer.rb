a276e6e71aa83f2744cbb859abbbeb8070bfe0ab
yyu8k <yyu8k@icloud.com>
2019-11-27 18:50:56 +0900

    add:designate-destinations-and-subjects

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -1,2 +1,12 @@
 class InquiryMailer < ApplicationMailer
+
+  def send_mail(inquiry)
+    @inquiry = inquiry
+    mail(
+      from: 'system@example.com',
+      to:   'manager@example.com',
+      subject: 'お問い合わせ通知'
+    )
+  end
+
 end
