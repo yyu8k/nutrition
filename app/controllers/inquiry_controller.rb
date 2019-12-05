dbb2af141723af9e827ce0799f16e6f2edcab4ca
yyu8k <yyu8k@icloud.com>
2019-12-05 18:41:16 +0900

    update:inquiry_controller.rb-add-thanks-action

--- app/controllers/inquiry_controller.rb
+++ app/controllers/inquiry_controller.rb
@@ -18,4 +18,13 @@ class InquiryController < ApplicationController
     end
   end
 
+  def thanks
+    # メール送信
+    @inquiry = Inquiry.new(params[:inquiry])
+    InquiryMailer.received_email(@inquiry).deliver
+
+    # 完了画面を表示
+    render :action => 'thanks'
+  end
+
 end
