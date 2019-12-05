2bb3627f9fad93c9a2c7ba7f7a7e53cc83c671b1
yyu8k <yyu8k@icloud.com>
2019-12-05 18:37:54 +0900

    update:routes.rb-for-inquiry

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,6 @@ Rails.application.routes.draw do
   end
   resources :tweets, only: [:index]
   get 'inquiry' => 'inquiry#index'              # 入力画面
+  post 'inquiry/confirm' => 'inquiry#confirm'   # 確認画面
+  post 'inquiry/thanks' => 'inquiry#thanks'     # 送信完了画面
 end
