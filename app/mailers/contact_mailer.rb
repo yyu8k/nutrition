17e2b657eb411c8997539c5023fd0005da0b055f
yyu8k <yyu8k@icloud.com>
2019-12-12 19:53:12 +0900

    update:contact_mailer.rb-remove-comment-out

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,10 +1,5 @@
 class ContactMailer < ApplicationMailer
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.contact_mailer.contact_mail.subject
-  #
   def contact_mail
     @greeting = "Hi"
 
