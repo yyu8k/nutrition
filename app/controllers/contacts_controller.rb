3e54670467e90f00de9ecd49fefc97c38e9f5cde
yyu8k <yyu8k@icloud.com>
2019-12-13 23:48:46 +0900

    add:strong-parameters

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -18,7 +18,7 @@ class ContactsController < ApplicationController
   private
 
   def contact_params
-    params.require(:contact).permit(:email, :message)
+    params.require(:contact).permit(:name, :email, :message)
   end
 
 end
