b6526c8acfce398200481261da42f8e2db6c9813
yyu8k <yyu8k@icloud.com>
2020-03-19 20:42:22 +0900

    Delete 20190904110804_create_records.rb

--- db/migrate/20190904110804_create_records.rb
+++
@@ -1,9 +0,0 @@
-class CreateRecords < ActiveRecord::Migration[5.2]
-  def change
-    create_table :records do |t|
-      t.integer    :user_id
-      t.date       :date
-      t.timestamps
-    end
-  end
-end
